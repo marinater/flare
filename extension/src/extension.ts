11a50fa6289a1b1a794641b48e7210f5fb11c74d
Samarth Patel <samarthxpatel@gmail.com>
2020-12-28 00:16:26 -0500

    Added support for receiving attachments and images; Feature set complete!!!
    Co-authored-by: Derek Li <derekli2011@gmail.com>

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,188 +1,186 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as fs from 'fs';
-import * as path from 'path';
-import * as querystring from 'querystring';
-import { v4 as uuidv4 } from 'uuid';
-import * as vscode from 'vscode';
+import * as fs from 'fs'
+import * as path from 'path'
+import * as querystring from 'querystring'
+import { v4 as uuidv4 } from 'uuid'
+import * as vscode from 'vscode'
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3000'
 // const BASE_URL = 'https://discord-flare.herokuapp.com';
 
-type Account = {
-    signedIn: true
-    email: string
-    sessionID: string
-    expiration: Date
-} | {
-    signedIn: false
-};
+type Account =
+	| {
+			signedIn: true
+			email: string
+			sessionID: string
+			expiration: Date
+	  }
+	| {
+			signedIn: false
+	  }
 
-let vscodeState = uuidv4();
+let vscodeState = uuidv4()
 
 const validateQueryParams = (data: any) => {
-    let account: Account = { signedIn: false };
+	let account: Account = { signedIn: false }
 
-    if (!data.email || !data.sessionID || !data.vscode_state || !data.expiration) {
-        return account;
-    }
+	if (!data.email || !data.sessionID || !data.vscode_state || !data.expiration) {
+		return account
+	}
 
-    if (vscodeState !== data.vscode_state) {
-        return account;
-    }
+	if (vscodeState !== data.vscode_state) {
+		return account
+	}
 
-    const expiration = new Date(data.expiration);
-    if (expiration < new Date()) {
-        return account;
-    }
+	const expiration = new Date(data.expiration)
+	if (expiration < new Date()) {
+		return account
+	}
 
-    account = {
-        signedIn: true,
-        email: String(data.email),
-        sessionID: String(data.sessionID),
-        expiration
-    };
+	account = {
+		signedIn: true,
+		email: String(data.email),
+		sessionID: String(data.sessionID),
+		expiration,
+	}
 
-    return account;
-};
+	return account
+}
 
 class AccountOps {
-    flare: Flare;
+	flare: Flare
 
-    constructor(flare: Flare) {
-        this.flare = flare;
-    }
+	constructor(flare: Flare) {
+		this.flare = flare
+	}
 
-    storeAccount = (account: Account) => {
-        const filePath = vscode.Uri.file(path.join(this.flare.context.extensionPath, 'account.json'));
-        this.flare.account = account;
+	storeAccount = (account: Account) => {
+		const filePath = vscode.Uri.file(path.join(this.flare.context.extensionPath, 'account.json'))
+		this.flare.account = account
 
-        try {
-            console.log('wrote to file ' + JSON.stringify(account, null, 4));
-            fs.writeFileSync(filePath.fsPath, JSON.stringify(account, null, 4));
-            return true;
-        }
-        catch {
-            vscode.window.showErrorMessage(`Could not persist session to ${filePath.fsPath}`);
-            return false;
-        }
-    };
+		try {
+			console.log('wrote to file ' + JSON.stringify(account, null, 4))
+			fs.writeFileSync(filePath.fsPath, JSON.stringify(account, null, 4))
+			return true
+		} catch {
+			vscode.window.showErrorMessage(`Could not persist session to ${filePath.fsPath}`)
+			return false
+		}
+	}
 
-    restoreAccount = (): Account => {
-        const filePath = vscode.Uri.file(path.join(this.flare.context.extensionPath, 'account.json'));
+	restoreAccount = (): Account => {
+		const filePath = vscode.Uri.file(path.join(this.flare.context.extensionPath, 'account.json'))
 
-        try {
-            const document = fs.readFileSync(filePath.fsPath, 'utf8');
-            return validateQueryParams(JSON.parse(document));
-        }
-        catch {
-            const account: Account = { signedIn: false };
-            this.storeAccount(account);
-            return account;
-        }
-    };
+		try {
+			const document = fs.readFileSync(filePath.fsPath, 'utf8')
+			return validateQueryParams(JSON.parse(document))
+		} catch {
+			const account: Account = { signedIn: false }
+			this.storeAccount(account)
+			return account
+		}
+	}
 
-    uriAuthHandler = (callback: () => void) => (uri: vscode.Uri) => {
-        const queryParams = querystring.decode(uri.query);
+	uriAuthHandler = (callback: () => void) => (uri: vscode.Uri) => {
+		const queryParams = querystring.decode(uri.query)
 
-        const account = validateQueryParams(queryParams);
-        if (!account.signedIn) {
-            vscode.window.showErrorMessage('Authentication callback failed');
-        }
+		const account = validateQueryParams(queryParams)
+		if (!account.signedIn) {
+			vscode.window.showErrorMessage('Authentication callback failed')
+		}
 
-        vscodeState = uuidv4();
-        this.storeAccount(account);
+		vscodeState = uuidv4()
+		this.storeAccount(account)
 
-        vscode.window.showInformationMessage('Flare authentication complete!');
+		vscode.window.showInformationMessage('Flare authentication complete!')
 
-        callback();
-    };
+		callback()
+	}
 
-    signIn = () => {
-        vscode.env.openExternal(vscode.Uri.parse(`${BASE_URL}/auth/vscode?vscode_state=${vscodeState}`));
-    };
+	signIn = () => {
+		vscode.env.openExternal(vscode.Uri.parse(`${BASE_URL}/auth/vscode?vscode_state=${vscodeState}`))
+	}
 
-    signOut = () => {
-        vscode.window.showInformationMessage('logout complete');
-        this.storeAccount({ signedIn: false });
-    };
+	signOut = () => {
+		vscode.window.showInformationMessage('logout complete')
+		this.storeAccount({ signedIn: false })
+	}
 }
 
 class Flare {
-    context: vscode.ExtensionContext;
-    account: Account;
-    accountOps: AccountOps;
+	context: vscode.ExtensionContext
+	account: Account
+	accountOps: AccountOps
 
-    constructor(context: vscode.ExtensionContext) {
-        this.context = context;
-        this.accountOps = new AccountOps(this);
-        this.account = this.accountOps.restoreAccount();
-    }
+	constructor(context: vscode.ExtensionContext) {
+		this.context = context
+		this.accountOps = new AccountOps(this)
+		this.account = this.accountOps.restoreAccount()
+	}
 
-    open = () => {
-        if (!this.account.signedIn) {
-            this.login();
-            return;
-        }
+	open = () => {
+		if (!this.account.signedIn) {
+			this.login()
+			return
+		}
 
-        const panel = vscode.window.createWebviewPanel(
-            'flare', // Identifies the type of the webview. Used internally
-            'Flare', // Title of the panel displayed to the user
-            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-            {
-                enableScripts: true,
-                localResourceRoots: [
-                    vscode.Uri.file(path.join(this.context.extensionPath, 'views'))
-                ]
-            }
-        );
+		const panel = vscode.window.createWebviewPanel(
+			'flare', // Identifies the type of the webview. Used internally
+			'Flare', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true,
+				localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'views'))],
+			}
+		)
 
-        const filePath = vscode.Uri.file(path.join(this.context.extensionPath, 'views', 'index.html'));
+		const filePath = vscode.Uri.file(path.join(this.context.extensionPath, 'views', 'index.html'))
 
-        let webViewHTML = fs.readFileSync(filePath.fsPath, 'utf8');
-        webViewHTML = webViewHTML.replace(/{{sessionID}}/g, this.account.sessionID);
-        webViewHTML = webViewHTML.replace(/{{base_url}}/g, BASE_URL);
+		let webViewHTML = fs.readFileSync(filePath.fsPath, 'utf8')
+		webViewHTML = webViewHTML.replace(/{{sessionID}}/g, this.account.sessionID)
 
-        panel.webview.html = webViewHTML;
-    };
+		webViewHTML = webViewHTML.replace(/{{base_url}}/g, BASE_URL)
 
-    login = () => {
-        if (!this.account.signedIn) {
-            vscode.window.showInformationMessage('Please open the link and sign in to Github');
-            this.accountOps.signIn();
-        }
-        else {
-            vscode.window.showInformationMessage('Signed in as ' + this.account.email);
-            this.open();
-        }
-    };
+		const onDiskViewsPath = vscode.Uri.file(path.join(this.context.extensionPath, 'views'))
+		const webURIViewsPath = panel.webview.asWebviewUri(onDiskViewsPath).toString()
 
-    logout = () => {
-        if (this.account.signedIn) {
-            this.accountOps.signOut();
-            vscode.window.showInformationMessage('Signed out of Flare');
-        }
-        else {
-            vscode.window.showInformationMessage('You are already signed out of Flare');
-        }
-    };
+		webViewHTML = webViewHTML.replace(/{{views_path}}/g, webURIViewsPath)
+
+		panel.webview.html = webViewHTML
+	}
+
+	login = () => {
+		if (!this.account.signedIn) {
+			vscode.window.showInformationMessage('Please open the link and sign in to Github')
+			this.accountOps.signIn()
+		} else {
+			vscode.window.showInformationMessage('Signed in as ' + this.account.email)
+			this.open()
+		}
+	}
+
+	logout = () => {
+		if (this.account.signedIn) {
+			this.accountOps.signOut()
+			vscode.window.showInformationMessage('Signed out of Flare')
+		} else {
+			vscode.window.showInformationMessage('You are already signed out of Flare')
+		}
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    const flare = new Flare(context);
+	const flare = new Flare(context)
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('flare.logout', flare.logout)
-    );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('flare.open', flare.open)
-    );
+	context.subscriptions.push(vscode.commands.registerCommand('flare.logout', flare.logout))
+	context.subscriptions.push(vscode.commands.registerCommand('flare.open', flare.open))
 
-    context.subscriptions.push(
-        vscode.window.registerUriHandler({
-            handleUri: flare.accountOps.uriAuthHandler(flare.open)
-        })
-    );
+	context.subscriptions.push(
+		vscode.window.registerUriHandler({
+			handleUri: flare.accountOps.uriAuthHandler(flare.open),
+		})
+	)
 }
 
 export function deactivate() {}
---
+++ extension/views/index.js
@@ -0,0 +1,285 @@
+'use strict;';
+
+let currentChannel = null;
+let currentGuild = null;
+let localMessageStorage = {};
+
+const setActiveGuild = () => {
+	document.querySelectorAll('.guild-active').forEach(elem => {
+		if (elem.id !== `guild-${currentGuild.id}`) {elem.classList.remove('guild-active');}
+	});
+	document.getElementById(`guild-${currentGuild.id}`).classList.add('guild-active');
+};
+
+const setActiveChannel = () => {
+	document.querySelectorAll('.channel-container-active').forEach(elem => {
+		if (elem.id !== `channel-${currentChannel.id}`) {elem.classList.remove('channel-container-active');}
+	});
+	document.getElementById(`channel-${currentChannel.id}`).classList.add('channel-container-active');
+};
+
+const buildFromTemplate = (template, templateValues) => {
+	const container = document.createElement('div');
+	container.innerHTML = template;
+
+	for (let query in templateValues) {
+		const elem = container.querySelector(query);
+		elem.innerText = templateValues[query];
+	}
+
+	return container;
+};
+
+const createMessage = (message, displayHeader) => {
+	const template = `
+                <div class='message-container'>
+                    <div class='message-avatar-container'>
+                        <img class='message-avatar-img'>
+                    </div>
+                    <div class='message-data-container'>
+                        <div class='message-info-row'>
+                            <span class='message-info-author'></span>
+                            <span class='message-info-timestamp'></span>
+                        </div>
+                        <div class='message-content-row'>
+                        </div>
+                        <div class="message-attachment-container">
+                        </div>
+                    </div>
+                </div>
+            `;
+
+	const elem = buildFromTemplate(template, {
+		'.message-info-author': message.author.name,
+		'.message-info-timestamp': dayjs(message.timestamp).format('MM/DD/YY [at] h:mm A'),
+		'.message-content-row': message.content,
+	});
+
+	if (displayHeader) {
+		const avatar = elem.querySelector('.message-avatar-img');
+		avatar.src = message.author.pfp;
+		elem.querySelector('.message-container').style.paddingTop = '15px';
+	} else {
+		elem.querySelector('.message-avatar-container').style.height = 'initial';
+		elem.querySelector('.message-avatar-img').remove();
+		elem.querySelector('.message-info-row').remove();
+	}
+
+    const imageAttachmentTemplate = `
+        <img class='message-attachment-image'/>
+    `;
+
+    const fileAttachmentTemplate = `
+        <div class='message-attachment-file'>
+            <a class='message-attachment-file-text'></a>
+        </div>
+    `;
+
+    const attachmentContainer = elem.querySelector('.message-attachment-container');
+    for (const attachment of message.attachments) {
+        let attachmentElem;
+        if (attachment.height === null || attachment.width === null) {
+            attachmentElem = buildFromTemplate(fileAttachmentTemplate, { '.message-attachment-file-text': attachment.name || '(File attachment)' });
+            const attachmentText = attachmentElem.querySelector('.message-attachment-file-text');
+            attachmentText.href = attachment.url;
+        }
+        else {
+            attachmentElem = buildFromTemplate(imageAttachmentTemplate, {});
+            const attachmentText = attachmentElem.querySelector('.message-attachment-image');
+            attachmentText.src = attachment.url;
+            attachmentText.style.height = `${attachment.height}px`;
+            attachmentText.style.width = `${attachment.width}px`;
+        }
+
+        attachmentContainer.appendChild(attachmentElem);
+    }
+
+
+
+	const messageList = document.getElementById('message-list');
+	const messageListTail = document.getElementById('message-list-tail');
+
+	messageList.insertBefore(elem, messageListTail);
+	messageList.scrollTop = messageList.scrollHeight;
+};
+
+const setGuilds = () => {
+	const container = document.getElementById('server-container');
+	container.innerHTML = '';
+
+	const template = `
+                <div class='guild-container'>
+                    <img class='guild-icon-img'>
+                    <span class='guild-name-text'>
+                    </span>
+                </div>
+            `;
+
+	for (const guild of Object.values(localMessageStorage)) {
+		const elem = buildFromTemplate(template, {
+			'.guild-name-text': guild.name
+				.split(' ')
+				.map(x => (x ? x[0] : ''))
+				.join(''),
+		});
+
+		if (!guild.icon) {
+			elem.querySelector('.guild-icon-img').remove();
+		} else {
+			elem.querySelector('.guild-icon-img').src = guild.icon;
+			elem.querySelector('.guild-name-text').remove();
+		}
+
+		const elemInner = elem.querySelector('.guild-container');
+		elemInner.id = `guild-${guild.id}`;
+
+		elemInner.onclick = () => {
+			currentGuild = guild;
+			const channelList = Object.values(guild.channels);
+			currentChannel = channelList.length > 0 ? channelList[0] : null;
+
+			setActiveGuild();
+			setChannels();
+			setChats();
+		};
+		container.appendChild(elem);
+	}
+
+	setActiveGuild();
+};
+
+const setChannels = () => {
+	const container = document.getElementById('channels-container');
+	container.innerHTML = '';
+
+	const headerTemplate = `
+                <div id='channels-header'>
+                    TEXT CHANNELS
+                </div>
+    `;
+
+    const header = buildFromTemplate(headerTemplate, {});
+	container.appendChild(header);
+
+	if (!currentGuild) {return;}
+
+	const channelTemplate = `
+                <div class='channel-container'>
+                    <span class='channel-name-text'></span>
+                </div>
+    `;
+
+	for (const channel of Object.values(currentGuild.channels)) {
+		const elem = buildFromTemplate(channelTemplate, {
+			'.channel-name-text': `# ${channel.name}`,
+		});
+
+		const channelContainer = elem.querySelector('.channel-container');
+		channelContainer.id = `channel-${channel.id}`;
+
+		channelContainer.onclick = () => {
+			currentChannel = channel;
+
+			setActiveChannel();
+			document.getElementById('message-input').placeholder = `Message #${channel.name}`;
+			setChats();
+		};
+
+		container.appendChild(elem);
+	}
+
+	setActiveChannel();
+};
+
+const setChats = () => {
+	document.getElementById('message-list').innerHTML = '<a id="message-list-tail"></a>';
+
+	if (!currentChannel) {return;}
+
+	let prevAuthor = null;
+	for (const message of currentChannel.messages) {
+		createMessage(message, message.author.id !== prevAuthor);
+		prevAuthor = message.author.id;
+	}
+};
+
+const inputField = document.getElementById('message-input');
+
+inputField.onkeydown = event => {
+	if (event.keyCode !== 13) {return;}
+	if (!currentChannel && !currentGuild) {
+		return;
+	}
+
+	document.getElementById('message-input');
+	const retMessage = {
+		channelID: currentChannel.id,
+		guildID: currentGuild.id,
+		content: inputField.value,
+	};
+
+	socket.emit('message-post', retMessage);
+	inputField.value = '';
+};
+
+const handleMessage = data => {
+    const guild = localMessageStorage[data.guildID];
+    if (!guild) {return;}
+
+	const channel = guild.channels[data.channelID];
+    if (!channel) {return;}
+
+
+	const messages = channel.messages;
+	prevAuthor = messages.length > 0 ? messages[messages.length - 1].author.id : null;
+	messages.push(data);
+
+	if (currentChannel && data.channelID === currentChannel.id) {
+        createMessage(data, data.author.id !== prevAuthor);
+    }
+};
+
+socket.on('forward-message', handleMessage);
+socket.on('socket-init', data => {
+    localMessageStorage = {};
+
+    console.log(data);
+	for (const guild of data.guilds) {
+		localMessageStorage[guild.id] = {
+			id: guild.id,
+			name: guild.name,
+			icon: guild.icon,
+			channels: {},
+		};
+
+		for (const channel of guild.channels) {
+			localMessageStorage[guild.id].channels[channel.id] = {
+				id: channel.id,
+				name: channel.name,
+				messages: [],
+			};
+		}
+	}
+
+	const guildsList = Object.values(localMessageStorage);
+	currentGuild = guildsList.length > 0 ? guildsList[0] : null;
+
+	if (currentGuild) {
+		const channelsList = Object.values(currentGuild.channels);
+		currentChannel = channelsList.length > 0 ? channelsList[0] : null;
+	}
+
+	setGuilds();
+	setChannels();
+	setChats();
+});
+
+socket.onAny((event) => {
+    console.log(event);
+});
+
+socket.on('connect_error', err => {
+	console.error(`connect_error: ${err.message}`);
+});
+
+socket.emit('socket-init', '');
